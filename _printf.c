d474759775e970ceadabb6941032f92a69627e1d
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-21 02:05:27 +0100

    baghi gha nghawet

--- _printf.c
+++ _printf.c
@@ -21,96 +21,85 @@ int print_char(int a, int c, int b)
  *
  * Return: formatted strings
 */
-int print_string(char *str_char)
-{
-	int i = 0;
 
-	while (*str_char)
-	{
-		_putchar(*str_char);
-		str_char++;
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * format_c - A function that checks the format
- * @format: A pointer to a string
- * Return: -1
- */
-int format_c(const char *format)
-{
-	if (!format || (*format == '%' && !(format + 1)))
-	{
-		return (-1);
-	}
-	if (*format == '%' && *(format + 1) == ' ' && !(format + 2))
-	{
-		return (-1);
-	}
-	if (*format == '%')
-	{
-		format++;
-		while (*format)
-		{
-			if (*format == ' ')
-			{
-				format++;
-				continue;
-			}
-			return (0);
-		}
-		return (-1);
-	}
-	return (0);
-}
-
-/**
- * _printf - takes strings and arguments of each '%'
- * @format: the first string containing '%'
- * @...: list of arguments variable
- * Return: the total of character printed
-*/
 int _printf(const char *format, ...)
 {
-	char c, *s;
-	int i = 0;
-	va_list ap;
+    va_list args;
+    va_start(args, format);
 
-	va_start(ap, format);
-	if (format_c(format) == -1)
-		return (-1);
-	if (format)
-	{
-		while (*format)
-		{
-			if (*format == '%')
-			{
-				format++;
-				switch (*format)
-				{
-				case 'c':
-					c = va_arg(ap, int);
-					i += _putchar(c);
-					break;
-				case 's':
-					s = va_arg(ap, char*);
-					i += s ? print_string(s) : print_string("(null)");
-					break;
-				case '%':
-					i += _putchar('%');
-					break;
-				default:
-					i += _putchar('%') + _putchar(*format);
-					break;
-				}
-			}
-			else
-				i += _putchar(*format);
-			format++;
-			}
-		}
-		va_end(ap);
-		return (i);
-	}
+    int count = 0;
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    while (*str)
+                    {
+                        putchar(*str);
+                        count++;
+                        str++;
+                    }
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                case 'd':
+                case 'i':
+                {
+                    int num = va_arg(args, int);
+                    if (num < 0)
+                    {
+                        putchar('-');
+                        count++;
+                        num = -num;
+                    }
+                    int temp = num, digits = 0;
+                    while (temp)
+                    {
+                        temp /= 10;
+                        digits++;
+                    }
+                    temp = digits;
+                    while (digits--)
+                    {
+                        int power = 1;
+                        for (int i = 1; i < temp; i++)
+                            power *= 10;
+                        putchar('0' + (num / power));
+                        num %= power;
+                        temp--;
+                        count++;
+                    }
+                    break;
+                }
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
