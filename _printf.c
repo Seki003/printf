0f1cbba3e5f4d16469a31d660fa639c65c2d9b8f
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-21 22:40:48 +0100

    test

--- _printf.c
+++ _printf.c
@@ -1,116 +1,54 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * print_char - fucnction that prints a char character
- * @a: file descriptor integer to output
- * @c: character parameter/
- * @b: character size
- * Return: the character
- */
-int print_char(int a, int c, int b)
-{
-	int display;
-
-	display = write(a, &c, b);
-	return (display);
-}
-
-/**
- * print_string - Function which prints string
- * @str_char: pointer to char string
- *
- * Return: formatted strings
+ * _printf - take text as input and print it, clone printf
+ * @format: containts the strings and char
+ * @...: arguments entered
+ * Return: the number of char printed
 */
-int print_string(char *str_char)
-{
-	int i = 0;
 
-	while (*str_char)
-	{
-		_putchar(*str_char);
-		str_char++;
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * format_c - A function that checks the format
- * @format: A pointer to a string
- * Return: -1
- */
-int format_c(const char *format)
-{
-	if (!format || (*format == '%' && !(format + 1)))
-	{
-		return (-1);
-	}
-	if (*format == '%' && *(format + 1) == ' ' && !(format + 2))
-	{
-		return (-1);
-	}
-	if (*format == '%')
-	{
-		format++;
-		while (*format)
-		{
-			if (*format == ' ')
-			{
-				format++;
-				continue;
-			}
-			return (0);
-		}
-		return (-1);
-	}
-	return (0);
-}
-
-/**
- * _printf - takes strings and arguments of each '%'
- * @format: the first string containing '%'
- * @...: list of arguments variable
- * Return: the total of character printed
-*/
 int _printf(const char *format, ...)
 {
-	char c, *s;
+	va_list args;
 	int i = 0;
-	va_list ap;
+	int k = 0;
+	char *word = NULL;
+	int count = 0;
 
-	va_start(ap, format);
-	if (format_c(format) == -1)
-		return (-1);
-	if (format)
+	va_start(args, format);
+
+	while (format[i] != '\0')
 	{
-		while (*format)
+		if (format[i] == '%')
 		{
-			if (*format == '%')
+			i++;
+			if (format[i] == '%')
 			{
-				format++;
-				switch (*format)
-				{
-				case 'c':
-					c = va_arg(ap, int);
-					i += _putchar(c);
-					break;
-				case 's':
-					s = va_arg(ap, char*);
-					i += s ? print_string(s) : print_string("(null)");
-					break;
-				case '%':
-					i += _putchar('%');
-					break;
-				default:
-					i += _putchar('%') + _putchar(*format);
-					break;
-				}
+				_putchar('%');
+				count++;
+				i++;
 			}
-			else
-				i += _putchar(*format);
-			format++;
+			else if (format[i] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				count++;
+				i++;
+			}
+			else if (format[i] == 's')
+			{
+				i++;
+				count += print_str(va_arg(args, char *));
 			}
 		}
-		va_end(ap);
-		return (i);
+		else
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
 	}
+
+	va_end(args);
+	return (count);
+}
---
+++ print_str.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_string - func that print a string after specifier %
+ * @word: pointer to char string
+ *
+ * Return: formatted strings
+*/
+
+int print_str(char *word)
+{
+	int k = 0;
+
+	while (word[k])
+	{
+		putchar(word[k]);
+		k++;
+	}
+	return (k);
+}
