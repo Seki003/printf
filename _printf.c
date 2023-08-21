ae19a8d2c86151a0b24fd334d5ffa7940d87af26
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-21 02:39:22 +0100

    baghi gha nghawet

--- _printf.c
+++ _printf.c
@@ -1,70 +1,5 @@
 #include "main.h"
-
-/**
- * print_char - fucnction that prints a char character
- * @a: file descriptor integer to output
- * @c: character parameter/
- * @b: character size
- * Return: the character
- */
-int print_char(int a, int c, int b)
-{
-	int display;
-
-	display = write(a, &c, b);
-	return (display);
-}
-
-/**
- * print_string - Function which prints string
- * @str_char: pointer to char string
- *
- * Return: formatted strings
-*/
-int print_string(char *str_char)
-{
-	int i = 0;
-
-	while (*str_char)
-	{
-		_putchar(*str_char);
-		str_char++;
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * format_c - A function that checks the format
- * @format: A pointer to a string
- * Return: -1
- */
-int format_c(const char *format)
-{
-	if (!format || (*format == '%' && !(format + 1)))
-	{
-		return (-1);
-	}
-	if (*format == '%' && *(format + 1) == ' ' && !(format + 2))
-	{
-		return (-1);
-	}
-	if (*format == '%')
-	{
-		format++;
-		while (*format)
-		{
-			if (*format == ' ')
-			{
-				format++;
-				continue;
-			}
-			return (0);
-		}
-		return (-1);
-	}
-	return (0);
-}
+#define BUFFER_SIZE 1024
 
 /**
  * _printf - takes strings and arguments of each '%'
@@ -72,45 +7,126 @@ int format_c(const char *format)
  * @...: list of arguments variable
  * Return: the total of character printed
 */
+
+int print_positive_int(int value);
+
+void print_buffer(char buffer[], int *buff_ind);
+
+
 int _printf(const char *format, ...)
 {
-	char c, *s;
-	int i = 0;
-	va_list ap;
+    va_list args;
+    int char_count = 0;
+    int printed_chars = 0;
+    int buff_index = 0;
+    char char_buffer[BUFFER_SIZE];
 
-	va_start(ap, format);
-	if (format_c(format) == -1)
-		return (-1);
-	if (format)
-	{
-		while (*format)
-		{
-			if (*format == '%')
-			{
-				format++;
-				switch (*format)
-				{
-				case 'c':
-					c = va_arg(ap, int);
-					i += _putchar(c);
-					break;
-				case 's':
-					s = va_arg(ap, char*);
-					i += s ? print_string(s) : print_string("(null)");
-					break;
-				case '%':
-					i += _putchar('%');
-					break;
-				default:
-					i += _putchar('%') + _putchar(*format);
-					break;
-				}
-			}
-			else
-				i += _putchar(*format);
-			format++;
-			}
-		}
-		va_end(ap);
-		return (i);
-	}
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            if (*format == '%')
+            {
+                char_buffer[buff_index++] = '%';
+                if (buff_index == BUFFER_SIZE)
+                {
+                    print_buffer(char_buffer, &buff_index);
+                    char_count += buff_index;
+                }
+            }
+            else if (*format == 'c')
+            {
+                int ch = va_arg(args, int);
+                char_buffer[buff_index++] = ch;
+                if (buff_index == BUFFER_SIZE)
+                {
+                    print_buffer(char_buffer, &buff_index);
+                    char_count += buff_index;
+                }
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                while (*str)
+                {
+                    char_buffer[buff_index++] = *str;
+                    str++;
+                    if (buff_index == BUFFER_SIZE)
+                    {
+                        print_buffer(char_buffer, &buff_index);
+                        char_count += buff_index;
+                    }
+                }
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int value = va_arg(args, int);
+                printed_chars = print_positive_int(value);
+                char_count += printed_chars;
+            }
+        }
+        else
+        {
+            char_buffer[buff_index++] = *format;
+            if (buff_index == BUFFER_SIZE)
+            {
+                print_buffer(char_buffer, &buff_index);
+                char_count += buff_index;
+            }
+        }
+
+        format++;
+    }
+
+    print_buffer(char_buffer, &buff_index);
+    char_count += buff_index;
+
+    va_end(args);
+
+    return char_count;
+}
+
+int print_positive_int(int value)
+{
+    char num_buffer[32];
+    int printed_chars = 0;
+
+    if (value < 0)
+    {
+        putchar('-');
+        printed_chars++;
+        value = -value;
+    }
+
+    int index = 0;
+    do
+    {
+        num_buffer[index++] = '0' + (value % 10);
+        value /= 10;
+        printed_chars++;
+    } while (value > 0);
+
+    for (int j = index - 1; j >= 0; j--)
+    {
+        putchar(num_buffer[j]);
+    }
+
+    return printed_chars;
+}
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+    {
+        for (int i = 0; i < *buff_ind; i++)
+        {
+            putchar(buffer[i]);
+        }
+    }
+
+    *buff_ind = 0;
+}
